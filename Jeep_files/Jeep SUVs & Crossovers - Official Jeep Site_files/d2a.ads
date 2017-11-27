f2c617fd0395bc94939185f523b500a3afadc231
Claudia Rangel <crl2316@gmail.com>
2017-11-27 15:25:46 -0600

    added nav bar, jumbotron, left sidebar

---
+++ Jeep_files/Jeep SUVs & Crossovers - Official Jeep Site_files/d2a.ads
@@ -0,0 +1,25 @@
+/*
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+*/
+(function(){var b={d:"",a:function(){if(b.d=="1"){var a=document.createElement("iframe"),c=document.getElementsByTagName("script")[0],e="https://a01.korrelate.net/a/e/d2r.ads?et=r&adsuu=GXJAEOG4R0XQ&"+(new Date).getTime();a.width=0;a.height=0;a.frameBorder=0;a.border=0;c.parentNode.insertBefore(a,c);setTimeout(function(){a.src=e},1)}}},d={c:"",b:function(){if(d.c!="oo")(new Image).src="https://tapestry.tapad.com/tapestry/1?ta_partner_id=1866&ta_format=png&ta_partner_did=GXJAEOG4R0XQ"}};
+d.b();b.a()})();
